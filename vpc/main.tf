e6d4b27f6a6936dd13d333ec46f776627ec56a72
Drasti08 <drashtikaurav@gmail.com>
2024-06-08 12:48:54 +0530

    added multi-region for AWS cloud

--- vpc/main.tf
+++ vpc/main.tf
@@ -9,10 +9,23 @@ terraform {
 
 # Configure the AWS Provider
 provider "aws" {
+  alias = "us-east-1-region" #added multiple region in AWS
   region = "us-east-1"
 }
+
+provider "aws" {
+  alias = "us-west-2-region" #added multiple region in AWS
+  region = "us-west-2"
+}
+
 
 # Create a VPC
 resource "aws_vpc" "example" {
   cidr_block = "10.0.0.0/16"
+  provider = "aws.us-east-1-region" #tells aws to create a vpc in aws.<alias-name>, here us-east-1-region in provider block
+}
+
+resource "aws_vpc" "example" {
+  cidr_block = "10.0.0.1/16"
+  provider = "aws.us-west-2-region" #tells aws to create a vpc in aws.<alias-name>, here us-west-2-region in provider block
 }
