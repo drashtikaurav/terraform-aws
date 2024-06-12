53fe4b8890e765e4f59d63a27a7dbe6afbcbc0ca
Drasti08 <drashtikaurav@gmail.com>
2024-06-12 17:56:38 +0530

    added modules for ec2 and s3

--- modules/main.tf
+++ modules/main.tf
@@ -4,8 +4,13 @@ provider "aws" {
 
 module "ec2" {
   source = "./resource-modules/ec2-instance"
+  # ami-id = local.ami-id
+  # instance-type = local.instance-type
+  ami-id = var.ami-id
+  instance-type = var.instance-type
 }
 
 module "s3" {
   source = "./resource-modules/s3-bucket"
+  bucket-name = local.bucket-name
 }
